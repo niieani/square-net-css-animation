b40fdc8543f020dec4ba5d178781664ec36a1809
Bazyli Brzóska <bazyli.brzoska@gmail.com>
2014-11-21 16:17:26 +0100

    invent.life first draft

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -16,6 +16,9 @@
  *  limitations under the License
  *
  */
+
+//var animationLines = [];
+
 (function () {
   'use strict';
 
@@ -24,6 +27,7 @@
   var navdrawerContainer = querySelector('.navdrawer-container');
   var body = document.body;
   var appbarElement = querySelector('.app-bar');
+  //var topHeaderElement = querySelector('.top-header');
   var menuBtn = querySelector('.menu');
   var main = querySelector('main');
 
@@ -31,11 +35,13 @@
     body.classList.remove('open');
     appbarElement.classList.remove('open');
     navdrawerContainer.classList.remove('open');
+    //topHeaderElement.classList.remove('open');
   }
 
   function toggleMenu() {
     body.classList.toggle('open');
     appbarElement.classList.toggle('open');
+    //topHeaderElement.classList.toggle('open');
     navdrawerContainer.classList.toggle('open');
     navdrawerContainer.classList.add('opened');
   }
@@ -47,4 +53,225 @@
       closeMenu();
     }
   });
+
+  const TREE_THICKNESS = 2;
+
+  class TreeNode {
+    constructor(treeLine) {
+      this.lineElement = document.createElement('div');
+      this.size = 0;
+      this.treeLine = treeLine;
+      this.lineElement.className += this.treeLine.classes;
+      this.treeLine.treeElement.appendChild(this.lineElement);
+    }
+    extendLine() {
+      this.size += this.treeLine.extendBy;
+    }
+    get reversalMultiplier() {
+      return this.subtractMode ? -1 : 1;
+    }
+  }
+
+  class TreeVerticalNode extends TreeNode {
+    constructor(lifeTree, startX, startY, growDirection = 'up', prevDirection = 'up') {
+
+      super(lifeTree, startX, startY);
+
+      if (prevDirection === 'left' && growDirection === 'up')
+        this.subtractMode = true;
+
+      else if (prevDirection === 'up')
+        startY = startY - TREE_THICKNESS;
+
+      else if (prevDirection === 'right')
+        startX = startX - TREE_THICKNESS;
+
+      this.lineElement.style.left = startX + 'px';
+
+      this.initialX = startX;
+      this.initialY = startY;
+
+      this.lineElement.className += ' line vertical';
+      switch(growDirection)
+      {
+        case 'up':
+          this.lineElement.style.bottom = startY + 'px';
+          break;
+        case 'down':
+          this.lineElement.style.top = startY * this.reversalMultiplier + 'px'; //-
+          this.subtractMode = true;
+          break;
+      }
+    }
+    extendLine() {
+      super.extendLine();
+      this.lineElement.style.height = this.size + 'px';
+    }
+    get lastY() {
+      return this.size + this.initialY;
+    }
+    get lastX() {
+      return this.initialX;
+    }
+  }
+
+  class TreeHorizontalNode extends TreeNode {
+    constructor(lifeTree, startX, startY, growDirection = 'right', prevDirection = 'right') {
+
+      super(lifeTree, startX, startY);
+
+      if (prevDirection === 'up' && growDirection === 'left')
+      {
+        this.subtractMode = true;
+        startX = startX + TREE_THICKNESS;
+      }
+
+      if (prevDirection === 'left' && growDirection === 'left')
+        this.subtractMode = true;
+
+      else if (prevDirection === 'up')
+        startY = startY - TREE_THICKNESS;
+
+      else if (prevDirection === 'right')
+        startX = startX - TREE_THICKNESS;
+
+      this.initialX = startX;
+      this.initialY = startY;
+
+      this.lineElement.className += ' line horizontal';
+      this.lineElement.style.bottom = startY + 'px';
+
+      switch(growDirection)
+      {
+        case 'left':
+          this.initialX = startX;
+          this.lineElement.style.right = this.initialX * this.reversalMultiplier + 'px';
+          break;
+        case 'right':
+          this.lineElement.style.left = this.initialX + 'px';
+          break;
+      }
+
+    }
+    extendLine() {
+      super.extendLine();
+      this.lineElement.style.width = this.size + 'px';
+    }
+    get lastY() {
+      return this.initialY;
+    }
+    get lastX() {
+      return (this.size * this.reversalMultiplier + this.initialX);
+    }
+  }
+
+
+  function timeout(ms) {
+    return new Promise((resolve) => {
+      setTimeout(resolve, ms);
+    });
+  }
+
+  class TreeLine {
+    constructor(lifeTree, extendBy = 20, startX = 0, startY = 0, iterations = 60, classes = '', interval = 300) {
+      this.treeElement = lifeTree;
+      this.nodes = [];
+      this.extendBy = extendBy;
+      this.iterations = iterations;
+      this.interval = interval;
+      this.classes = classes;
+      this.types = ['up', 'left', 'right'];
+      this.addNode(startX, startY, 'up');
+    }
+    getRandomType(prevType) {
+      let nextType;
+      do nextType = this.types[Math.floor(Math.random() * this.types.length)];
+      while ((prevType === 'left' && nextType === 'right') || (prevType === 'right' && nextType === 'left') || (prevType === 'up' && nextType === 'up'));
+      return nextType;
+    }
+    async addNode(startX, startY, type = 'up', prevType = 'up') {
+      let treeLine = this;
+
+      let node;
+      switch (type)
+      {
+        case 'up':
+          node = new TreeVerticalNode(treeLine, startX, startY, 'up', prevType);
+          break;
+        case 'down':
+          node = new TreeVerticalNode(treeLine, startX, startY, 'down', prevType);
+          break;
+        case 'left':
+          node = new TreeHorizontalNode(treeLine, startX, startY, 'left', prevType);
+          break;
+        case 'right':
+          node = new TreeHorizontalNode(treeLine, startX, startY, 'right', prevType);
+          break;
+      }
+
+      //console.log('adding node X & Y', type, startX, startY, node);
+      this.nodes.push(node);
+
+      while (treeLine.nodes.length < treeLine.iterations) {
+        console.log(treeLine.nodes.length, treeLine.iterations);
+        let nextType = treeLine.getRandomType(type);
+        while (type === 'left' && nextType === 'left' || type === 'right' && nextType === 'right')
+        {
+          await timeout(treeLine.interval);
+          node.extendLine();
+          nextType = treeLine.getRandomType(type);
+          //treeLine.nodes[treeLine.nodes.length-1].extendLine();
+          //setTimeout(function(){
+          //  treeLine.addNode(node.lastX, node.lastY, nextType, type);
+          //}, 300);
+        }
+        await timeout(treeLine.interval);
+        node.extendLine();
+        await treeLine.addNode(node.lastX, node.lastY, nextType, type);
+      }
+      /*
+      this.generator = setTimeout(function(){
+        if (treeLine.nodes.length > treeLine.iterations){
+          //clearTimeout(treeLine.generator);
+          return;
+        }
+        node.extendLine();
+
+        let nextType = treeLine.getRandomType(type);
+        //let nextType = treeLine.nodes.size <= 4 ? (treeLine.nodes.size % 2 === 1 ? 'right' : 'up') : (treeLine.nodes.size % 2 === 1 ? 'left' : 'up');
+        //let nextType = 'left';
+        treeLine.addNode(node.lastX, node.lastY, nextType, type);
+      }, 300);
+      */
+    }
+  }
+
+  //async function startDelayed() {
+  //  await timeout(5000);
+    // LIFE TREE
+  var lifeTree = querySelector('.life-tree');
+  var treeLine = new TreeLine(lifeTree, 20, -50, 0, 70, 'color-dark-blue', 100);
+  var treeLine2 = new TreeLine(lifeTree, 20, 0, 0, 80, 'color-red', 100);
+  var treeLine3 = new TreeLine(lifeTree, 20, 50, 0, 70, 'color-violet', 100);
+  //}
+  //startDelayed();
+
+
+/*
+  var extendLine = function(){
+    treeLine.extendLine();
+  };
+  var lineExtending = setInterval(extendLine, 2000);
+*/
+  /*
+  var line = new TreeLine(lifeTree);
+
+  var extendLine = function(){
+    console.log(height);
+    height += 20;
+    line.style.height = height + 'px';
+  };
+  //var lineExtending = setInterval(extendLine, 2000);
+  */
+
 })();
